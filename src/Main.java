e1ee50b257b88b1394577d94d892d66eb75f4b8e
panyiwen <panyiwen@sensetime.com>
2018-10-25 22:26:16 +0800

    just a change 2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("just a change");
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("over!!!");
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
     }
 }
